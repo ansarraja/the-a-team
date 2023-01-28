547bbc46211d3921a8a583a3ef029124f6865f7e
Ansar Mehmood <ansar.mehmood@directlinegroup.co.uk>
2023-01-28 19:51:46 +0000

    added Manager, Engineer and itern Classes and get functions

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,10 +1,10 @@
-// TODO: Write code to define and export the Employee class
+// code to define and export the Employee class
 class Employee {
  constructor(name, id, email) {
   this.name = name;
   this.id = id;
   this.email = email;
-  const role = "Employee"
+
  }
 
  getName() {
@@ -20,7 +20,7 @@ class Employee {
  }
 
  getRole() {
-  return this.role;
+  return 'Employee';
  }
 }
 
